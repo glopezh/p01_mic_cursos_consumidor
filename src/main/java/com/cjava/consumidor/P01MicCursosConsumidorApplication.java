dbc8855c79f00e69bcab397ed7bb1a2c96510a90
Geraldine López <geraldinelopez@MacBook-Pro-de-Geraldine-2.local>
2024-08-22 03:33:25 +0200

    first commit

---
+++ src/main/java/com/cjava/consumidor/P01MicCursosConsumidorApplication.java
@@ -0,0 +1,13 @@
+package com.cjava.consumidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P01MicCursosConsumidorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P01MicCursosConsumidorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cjava/consumidor/P01MicCursosConsumidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjava.consumidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P01MicCursosConsumidorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
